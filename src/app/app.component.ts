3e77551f4978c1610be1947f32a63f62bae886ff
Aditya Loshali <adityaloshali1@gmail.com>
2018-03-14 04:22:47 +0530

    first commit. recipe and shopping list with firebase storage complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAoVF1GUvuG6ogx86GTbyaxZKWTK27rztI",
+      authDomain: "recipe-app-32c80.firebaseapp.com",
+    });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
